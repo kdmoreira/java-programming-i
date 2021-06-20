687830100a3e6dca4601f9de11d7b52fa20a53c9
Karina Moreira <kakatinosa@hotmail.com>
2021-06-19 22:37:45 -0300

    Add file

---
+++ mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class SumOfASequence {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Last number?");
+        int number = Integer.valueOf(scanner.nextLine());
+        int sum = 0;
+        for (int i = 0; i <= number; i++) {
+            sum = sum + i;
+        }
+        System.out.println("The sum is " + sum);
+
+    }
+}
