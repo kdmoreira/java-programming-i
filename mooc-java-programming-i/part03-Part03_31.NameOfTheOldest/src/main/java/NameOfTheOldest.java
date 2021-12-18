f37d305ec0d02d98b000e765a10e0128c423c5cb
Karina Moreira <kakatinosa@hotmail.com>
2021-12-18 19:46:47 -0300

    Add files

---
+++ mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class NameOfTheOldest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int oldest = -1;
+        String name = "";
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            String[] parts = input.split(",");
+            if (Integer.valueOf(parts[1]) > oldest) {
+                oldest = Integer.valueOf(parts[1]);
+                name = parts[0];
+            }
+        }
+        System.out.println("Name of the oldest: " + name);
+
+
+    }
+}
